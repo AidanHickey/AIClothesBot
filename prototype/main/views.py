0a2cc15923dd60412fb2cd5e78142f8f3668dc3c
Dai-V <dai.quach.756@my.csun.edu>
2024-11-24 18:33:01 -0800

    Pagination completed. Somebody please move the page count to center I don't know html/css.

--- prototype/main/views.py
+++ prototype/main/views.py
@@ -1,12 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Products
+import json
+from django.http import JsonResponse
+import requests
+from django.core.paginator import Paginator
 
 
 def index(request):
     return render(request, 'dashboard.html')
 
 def marketplace(request):
-    products = Products.objects.all()
+    response = requests.get("https://fakestoreapi.com/products/category/men's clothing")
+    data = response.json()
+    for p in data:
+        if not Products.objects.filter(productname=p['title'],price=p['price'],image=p['image'],category=p['category']):
+            b = Products(productname=p['title'],price=p['price'],image=p['image'],category=p['category'])
+            b.save()
 
+    response = requests.get("https://fakestoreapi.com/products/category/women's clothing")
+    data = response.json()
+    for p in data:
+        if not Products.objects.filter(productname=p['title'],price=p['price'],image=p['image'],category=p['category']):
+            b = Products(productname=p['title'],price=p['price'],image=p['image'],category=p['category'])
+            b.save()
+
+
+    product_list = Products.objects.all()
+    paginator = Paginator(product_list, 10)
+    page = request.GET.get('page')
+    products = paginator.get_page(page)
+    
     return render(request, 'marketplace.html', {'products':products})
